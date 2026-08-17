4e147edab7d1bacfe10f86c8a28ca7f1e2981df2
Pat Walters <wpwalters@gmail.com>
2026-08-17 17:39:29 -0400

    removed np.float

--- support_libs/delong_ci.py
+++ support_libs/delong_ci.py
@@ -17,7 +17,7 @@ def compute_midrank(x):
     J = np.argsort(x)
     Z = x[J]
     N = len(x)
-    T = np.zeros(N, dtype=np.float)
+    T = np.zeros(N, dtype=float)
     i = 0
     while i < N:
         j = i
@@ -25,7 +25,7 @@ def compute_midrank(x):
             j += 1
         T[i:j] = 0.5*(i + j - 1)
         i = j
-    T2 = np.empty(N, dtype=np.float)
+    T2 = np.empty(N, dtype=float)
     # Note(kazeevn) +1 is due to Python using 0-based indexing
     # instead of 1-based in the AUC formula in the paper
     T2[J] = T + 1
@@ -43,7 +43,7 @@ def compute_midrank_weight(x, sample_weight):
     Z = x[J]
     cumulative_weight = np.cumsum(sample_weight[J])
     N = len(x)
-    T = np.zeros(N, dtype=np.float)
+    T = np.zeros(N, dtype=float)
     i = 0
     while i < N:
         j = i
@@ -51,7 +51,7 @@ def compute_midrank_weight(x, sample_weight):
             j += 1
         T[i:j] = cumulative_weight[i:j].mean()
         i = j
-    T2 = np.empty(N, dtype=np.float)
+    T2 = np.empty(N, dtype=float)
     T2[J] = T
     return T2
 
@@ -85,9 +85,9 @@ def fastDeLong(predictions_sorted_transposed, label_1_count):
     negative_examples = predictions_sorted_transposed[:, m:]
     k = predictions_sorted_transposed.shape[0]
 
-    tx = np.empty([k, m], dtype=np.float)
-    ty = np.empty([k, n], dtype=np.float)
-    tz = np.empty([k, m + n], dtype=np.float)
+    tx = np.empty([k, m], dtype=float)
+    ty = np.empty([k, n], dtype=float)
+    tz = np.empty([k, m + n], dtype=float)
     for r in range(k):
         tx[r, :] = compute_midrank(positive_examples[r, :])
         ty[r, :] = compute_midrank(negative_examples[r, :])
